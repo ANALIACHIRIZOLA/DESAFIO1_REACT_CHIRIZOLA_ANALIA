936fee9f032d21e97a14548e8b766b3074df4519
Chirizola Analia <analiachirizola@hotmail.com>
2022-05-12 23:46:58 -0300

    desafio map

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
+import React from "react"
 import Navbar from "./components/Navbar/Navbar";
 import ItemListContainer from "./components/ItemListContainer/ItemListContainer"
+import {products} from "./data/productos"
+
 
 function App() {
+
+  const task = new Promise((resolve,reject) => {
+    setTimeout(() => {
+    resolve(products);
+    }, 2000)
+ 
+  })
+  task.then( result =>{
+    console.log(result)
+  })
+      
   return (
     <div className="App">
       <Navbar/>
+      {products.map((product) => {
+        return (
+          <div key={product.id}>
+          <p>{product.nombre}</p>
+          </div>
+        )
+        })}
       <ItemListContainer greeting={"mensaje de ok"}/>
     </div>
 
